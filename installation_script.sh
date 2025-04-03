df97165a506fc577aab9293ecec58ce3cc194b7f
RaymWong <46432420+RaymWong@users.noreply.github.com>
2025-04-03 18:12:04 +0800

    update installation script

--- installation_script.sh
+++ installation_script.sh
@@ -2,7 +2,7 @@
 set -e
 sudo apt update 
 sudo apt --yes install libtss2-* tpm-udev tpm2-abrmd tpm2-tools python-wxtools python3-pubsub awscli
-
+sudo usermod --append --groups tss $(whoami)
 cd $PWD/Python_TPM20_GUI/
 sudo chmod a+rwx create_binary_package.sh 
 ./create_binary_package.sh
