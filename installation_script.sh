acbac13a51e6e01f359ea6fdfc15ab753eede3ee
RaymWong <46432420+RaymWong@users.noreply.github.com>
2025-04-06 23:20:48 +0800

    Remove awscli package

--- installation_script.sh
+++ installation_script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 sudo apt update 
-sudo apt --yes install libtss2-* tpm-udev tpm2-abrmd tpm2-tools python-wxtools xxd python3-pubsub awscli
+sudo apt --yes install libtss2-* tpm-udev tpm2-abrmd tpm2-tools python-wxtools xxd python3-pubsub
 sudo usermod --append --groups tss $(whoami)
 cd $PWD/Python_TPM20_GUI/
 sudo chmod a+rwx create_binary_package.sh 
