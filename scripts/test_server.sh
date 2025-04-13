c0ac1385a1227c2190abdce6cd29e81632a34998
RaymWong <46432420+RaymWong@users.noreply.github.com>
2025-04-13 17:49:32 +0800

    Generate server key and certificate using TPM via provider

--- scripts/test_server.sh
+++ scripts/test_server.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 set -e
 
-openssl genpkey -config temp.cnf -provider tpm2 -algorithm RSA -out testkey.priv
+#~ #when auth value is empty
+#~ openssl genpkey -config temp.cnf -pkeyopt parent-auth: -provider tpm2 -algorithm RSA -out tpm_tss_key.pem
+
+#when auth value is set
+openssl genpkey -config temp.cnf -pkeyopt parent-auth:owner123 -provider tpm2 -algorithm RSA -out tpm_tss_key.pem
+
+
+openssl req -config temp.cnf -provider tpm2  -in tpm_tss_key.pem -pkeyopt parent-auth:owner123 -new -x509 -days 7300 -sha256   -out CA_rsa_cert.pem -subj '/C=SG/ST=Singapore/L=Singapore/O=Infineon Technologies/OU=DSS/CN=TPMEvalKitCA'
