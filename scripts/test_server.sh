19e0807ac869400f949e7d0b39b2f5ebfd3f5226
RaymWong <46432420+RaymWong@users.noreply.github.com>
2025-04-14 09:08:29 +0800

    WIP, resolving issue of CA signing certificate with openssl x509 via provider.

--- scripts/test_server.sh
+++ scripts/test_server.sh
@@ -5,7 +5,15 @@ set -e
 #~ openssl genpkey -config temp.cnf -pkeyopt parent-auth: -provider tpm2 -algorithm RSA -out tpm_tss_key.pem
 
 #when auth value is set
-openssl genpkey -config temp.cnf -pkeyopt parent-auth:owner123 -provider tpm2 -algorithm RSA -out tpm_tss_key.pem
+openssl genpkey -config temp.cnf -pkeyopt parent-auth:owner123 -provider tpm2 -algorithm RSA -out rsa_CA.tss
 
 
-openssl req -config temp.cnf -provider tpm2  -in tpm_tss_key.pem -pkeyopt parent-auth:owner123 -new -x509 -days 7300 -sha256   -out CA_rsa_cert.pem -subj '/C=SG/ST=Singapore/L=Singapore/O=Infineon Technologies/OU=DSS/CN=TPMEvalKitCA'
+openssl req -config temp.cnf -provider tpm2  -in rsa_CA.tss -pkeyopt parent-auth:owner123 -new -x509 -days 7300 -sha256   -out CA_rsa_cert.pem -subj '/C=SG/ST=Singapore/L=Singapore/O=Infineon Technologies/OU=DSS/CN=TPMEvalKitCA'
+
+openssl genpkey -config temp.cnf -pkeyopt parent-auth:owner123 -provider tpm2 -algorithm RSA -out rsa_server.tss
+
+
+openssl req -new -config temp.cnf -provider tpm2 -in rsa_server.tss -pkeyopt parent-auth:owner123 -subj /CN=TPM_UI/O=Infineon/C=SG -out server_rsa.csr
+
+
+OPENSSL_CONF=temp.cnf openssl x509 -req -provider tpm2 -provider base -in server_rsa.csr -CA CA_rsa_cert.pem -CAkey rsa_CA.tss  -out CAsigned_rsa_cert.crt -days 365 -sha256 -CAcreateserial 
