64a14a109a2ea606847ec25f61d35149fb32bf26
xinnyuuu <xinnyuuu>
2025-04-17 17:33:59 +0800

    New auth value update logic testing

--- Python_TPM20_GUI/tab3_provider.py
+++ Python_TPM20_GUI/tab3_provider.py
@@ -1046,9 +1046,8 @@ class Tab_RSA_MISC(wx.Panel):
         exec_cmd.execCLI(["rm", "rsa2.pem"])
         exec_cmd.execCLI(["rm", "mycipher"])
         exec_cmd.execCLI(["rm", "mysig"])
-        owner_auth = exec_cmd.get_auth_from_config('owner')
-        
-        if (owner_auth != ""):
+
+        if (exec_cmd.ownerAuth != ""):
             # Step 1: Evict control of the handle 0x81000008
             command_output = exec_cmd.execCLI([
                 "tpm2_evictcontrol", "-C", "o", "-P", "owner123", "-c", "0x81000008"
@@ -1092,7 +1091,6 @@ class Tab_RSA_MISC(wx.Panel):
 
             
         else:
-            self.command_out.AppendText("OwnerAuth found to be empty")
             command_output = exec_cmd.execCLI([
                 "openssl", "genpkey",
                 "-pkeyopt", "parent-auth:",
