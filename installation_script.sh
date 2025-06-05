74329e318b493f999ad37fd716586a16656bb30e
RaymWong <46432420+RaymWong@users.noreply.github.com>
2025-06-05 21:00:39 +0800

    Updated installation script

--- installation_script.sh
+++ installation_script.sh
@@ -26,49 +26,51 @@ cd bin/
 # Modify config.txt if it exists
 if [ -f "$CONFIG_FILENAME" ]; then
     echo "Found file $CONFIG_FILENAME."
-
     # Ensure both TPM overlays are present
-    for OVERLAY in "TPM_SPI_OVERLAY"; do
-	# Add SPI TPM overlay
-	if ! grep -q "$TPM_SPI_OVERLAY" "$CONFIG_FILENAME"; then
-		echo "$TPM_SPI_OVERLAY" | sudo tee -a "$CONFIG_FILENAME"
-		echo "Added '$TPM_SPI_OVERLAY' to $CONFIG_FILENAME."
-	fi
-	# Add I2C TPM overlay
-	if ! grep -q "$TPM_I2C_OVERLAY" "$CONFIG_FILENAME"; then
-		echo "$TPM_I2C_OVERLAY" | sudo tee -a "$CONFIG_FILENAME"
-		echo "Added '$TPM_I2C_OVERLAY' to $CONFIG_FILENAME."
-	fi
-    done
-
+    # Add SPI TPM overlay
+    if ! grep -q "$TPM_SPI_OVERLAY" "$CONFIG_FILENAME"; then
+	echo "$TPM_SPI_OVERLAY" | sudo tee -a "$CONFIG_FILENAME"
+	echo "Added '$TPM_SPI_OVERLAY' to $CONFIG_FILENAME."
+    else
+        echo "'$TPM_SPI_OVERLAY' exists in $CONFIG_FILENAME."
+    fi
+    # Add I2C TPM overlay
+    if ! grep -q "$TPM_I2C_OVERLAY" "$CONFIG_FILENAME"; then
+	echo "$TPM_I2C_OVERLAY" | sudo tee -a "$CONFIG_FILENAME"
+	echo "Added '$TPM_I2C_OVERLAY' to $CONFIG_FILENAME."
+    else
+        echo "'$TPM_I2C_OVERLAY' exists in $CONFIG_FILENAME."
+    fi
+    
     # Enable SPI by replacing 'off' with 'on', or appending if neither found
     if grep -q "$SPI_OVERLAY_ON" "$CONFIG_FILENAME"; then
-        echo "The string '$SPI_OVERLAY_ON' already exists in $CONFIG_FILENAME."
+        echo "'$SPI_OVERLAY_ON' already exists in $CONFIG_FILENAME."
     else
         if grep -q "$SPI_OVERLAY_OFF" "$CONFIG_FILENAME"; then
             sudo sed -i "s/$SPI_OVERLAY_OFF/$SPI_OVERLAY_ON/g" "$CONFIG_FILENAME"
             echo "Replaced '$SPI_OVERLAY_OFF' with '$SPI_OVERLAY_ON'."
         else
             echo "$SPI_OVERLAY_ON" | sudo tee -a "$CONFIG_FILENAME"
-            echo "The string '$SPI_OVERLAY_ON' has been appended to $CONFIG_FILENAME."
+            echo "'$SPI_OVERLAY_ON' has been appended to $CONFIG_FILENAME."
         fi
     fi
 
     # Enable I2C by replacing 'off' with 'on', or appending if neither found
     if grep -q "$I2C_OVERLAY_ON" "$CONFIG_FILENAME"; then
-        echo "The string '$I2C_OVERLAY_ON' already exists in $CONFIG_FILENAME."
+        echo "'$I2C_OVERLAY_ON' already exists in $CONFIG_FILENAME."
     else
         if grep -q "$I2C_OVERLAY_OFF" "$CONFIG_FILENAME"; then
             sudo sed -i "s/$I2C_OVERLAY_OFF/$I2C_OVERLAY_ON/g" "$CONFIG_FILENAME"
             echo "Replaced '$I2C_OVERLAY_OFF' with '$I2C_OVERLAY_ON'."
         else
             echo "$I2C_OVERLAY_ON" | sudo tee -a "$CONFIG_FILENAME"
-            echo "The string '$I2C_OVERLAY_ON' has been appended to $CONFIG_FILENAME."
+            echo "'$I2C_OVERLAY_ON' has been appended to $CONFIG_FILENAME."
         fi
     fi
 
 else
-    echo "The file $CONFIG_FILENAME does not exist."
+    echo "The file $CONFIG_FILENAME does not exist. Enable the SPI/I2C and TPM overlays manually needed"
+    exit
 fi
 
 sudo reboot
